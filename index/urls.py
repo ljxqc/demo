1e50ce57e5ff8548a1db55ce864575a81027fde9
ljxqc <ljxqc@ljxqc.com>
2020-11-16 18:18:28 +0800

    add views demo

--- index/urls.py
+++ index/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/', views.mydate),  # 带正则的URL
     # re_path('(?P<year>[0-9]{4})/', views.myyear, name='myyear'),  # 带名字的URL，可作HTML反向解析
     re_path('dict/(?P<year>[0-9]{4})/', views.myyear_dict, {'month': '05'}, name='myyear_dict', ),  # 设置额外参数（字典）
+    path('download.html', views.download),
+    path('login.html', views.login),
+    path('index/', views.ProductList.as_view()),
+    path('index/<id>.html', views.ProductList.as_view(), {'name': 'phone'})
 ]
--- index/views.py
+++ index/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+import csv
+from index.models import Product
+from django.views.generic import ListView
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('Hello World !')
+    # return HttpResponse('Hello World !')
+    # return render(request, 'index.html', context={'title': '首页'}, status=500)
+    # type_list = Product.objects.values('type').distinct()
+    # name_list = Product.objects.values('name', 'type')
+    # context = {'title': '首页', 'type_list': type_list, 'name_list': name_list}
+    # return render(request, 'index.html', context=context, status=200)
+    # return HttpResponse(str(2111))
+
+    type_list = Product.objects.values('type').distinct()
+    name_list = Product.objects.values('name', 'type')
+    title = '首页'
+    return render(request, 'index.html',context=locals(), status=200)  # 用locals() 代替context
+
 
 def mydate(request, year, month, day):
     return HttpResponse(f'{str(year)}/{str(month)}/{str(day)}')
@@ -14,3 +29,40 @@ def myyear(request, year):
 def myyear_dict(request, year, month):
     return render(request, 'myyear_dict.html', {'month': month})
 
+def download(request):
+    response = HttpResponse(content_type='text?csv')
+    response['Content-Disposition'] = 'attachment;filename="somefilename.csv"'
+    wirter = csv.writer(response)
+    wirter.writerow(['First row', 'A', 'B', 'C'])
+    return response
+
+def login(request):
+    # return redirect('/')
+    if request.method =='POST':
+        name = request.POST.get('name')
+        return redirect('/')
+    else:
+        if request.GET.get('name'):
+            name = request.GET.get('name')
+        else:
+            name = 'Everyone'
+        return HttpResponse('username is ' + name)
+    # return redirect('http://127.0.0.1:8000/')
+
+
+class ProductList(ListView):
+    context_object_name = 'type_list'
+    template_name = 'index_view.html'
+    queryset = Product.objects.values('type').distinct()
+
+    def get_queryset(self):
+        print(self.kwargs['id'])
+        print(self.kwargs['name'])
+        print(self.request.method)
+        type_list = Product.objects.values('type').distinct()
+        return type_list
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductList, self).get_context_data(**kwargs)
+        context['name_list'] = Product.objects.values('name', 'type')
+        return context
